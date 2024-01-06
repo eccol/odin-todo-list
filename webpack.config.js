d49ee448f9d4c034373f72f098fc460b26889cd5
Eric <126840543+eccol@users.noreply.github.com>
2024-01-06 14:56:14 -0500

    Set up webpack HTML server

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
+  devServer: {
+    static: './dist'
+  },
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  optimization: {
+    runtimeChunk: 'single'
   },
   module: {
     rules: [
@@ -15,5 +23,11 @@ module.exports = {
         use: ['style-loader', 'css-loader']
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      title: 'Todo List'
+    })
+  ]
 }
