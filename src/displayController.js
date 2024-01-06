c47662eff84ec19892485f604a350cbbf2faae98
Eric <126840543+eccol@users.noreply.github.com>
2024-01-06 15:31:27 -0500

    Properly nest todos in project

--- src/displayController.js
+++ src/displayController.js
@@ -1,22 +1,31 @@
 export default class DisplayController {
+  constructor() {
+    this.projectsContainer = document.querySelector('.projects-container');
+  }
+
   printProject(project) {
     const container = this.createElement('div');
+    container.classList.add('project-container');
     const heading = this.createElement('h1', project.title);
 
     container.appendChild(heading);
-    document.body.appendChild(container);
+    this.projectsContainer.appendChild(container);
 
-    for (let todo of project.todoItems) { this.printTodo(todo) };
+    for (let todo of project.todoItems) {
+      const todoElement = this.createTodoElement(todo);
+      container.appendChild(todoElement);
+    }
   }
 
-  printTodo(todo) {
+  createTodoElement(todo) {
     const container = this.createElement('div');
+    container.classList.add('todo-container');
     const heading = this.createElement('h2', todo.title);
     const body = this.createElement('p', todo.description);
 
     container.appendChild(heading);
     container.appendChild(body);
-    document.body.appendChild(container);
+    return container;
   }
 
   createElement(type, content) {
--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import DisplayController from './displayController.js';
 
 const defaultProject = new TodoProject('Default Project');
 const defaultTodo = new TodoItem('Default Todo', 'A default todo item', 'duedate', 'low');
+const testTodo = new TodoItem('another todo', 'descirpto', 'duedate', 'high');
 defaultProject.addTodo(defaultTodo);
+defaultProject.addTodo(testTodo);
 console.log(defaultProject);
 
 const displayController = new DisplayController;
