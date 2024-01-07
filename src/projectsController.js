abcb201260e62bb6c794b996a5d07b0cd9f8831c
Eric <126840543+eccol@users.noreply.github.com>
2024-01-07 12:04:45 -0500

    Update default tasks

--- src/projectsController.js
+++ src/projectsController.js
@@ -42,11 +42,16 @@ export default class ProjectsController {
   makeDefaultProject() {
     const tomorrow = new Date();
     tomorrow.setDate(tomorrow.getDate() + 1);
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
     this.createProject('Default Project');
-    const defaultTodo = new TodoTask('Default Todo', 'This task has no due date.', null, 'Low');
-    const testTodo = new TodoTask('Important Todo', 'This task has a due date and high priority.', tomorrow, 'High');
-    this.projects[0].addTask(defaultTodo);
-    this.projects[0].addTask(testTodo);
+    const default1 = new TodoTask('Default Task', 'This task has no due date.', null, 'Normal');
+    const default2 = new TodoTask('Important Task', 'This task has a due date and high priority.', tomorrow, 'High');
+    default2.toggleComplete();
+    const default3 = new TodoTask('Overdue Task', "This task's due date has passed.", yesterday, 'Low');
+    this.projects[0].addTask(default1);
+    this.projects[0].addTask(default2);
+    this.projects[0].addTask(default3);
   }
 
   getProjectById(id) {
